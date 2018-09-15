15e47b66991ed19d299227572d1eefaa2f6d15b8
sharmithaT <tsharmitha@gmail.com>
2018-09-14 22:34:06 -0400

    validations

--- src/main/java/com/onlinequiz/pro/controller/LoginController.java
+++ src/main/java/com/onlinequiz/pro/controller/LoginController.java
@@ -3,9 +3,10 @@ package com.onlinequiz.pro.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class LoginController {
     @Autowired
     LoginController loginController;
